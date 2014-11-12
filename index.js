121064733639271f77447c2fffeffb48206a2c34
Adriano Raiano <adriano@raiano.ch>
2014-11-12 12:28:15 +0100

    added manifest

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var lodash = require('lodash'),
     own_a = require('own_a');
+
+
+  var http = require('http');
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Just for CF! ;-)\n');
+  }).listen(process.env.PORT);
